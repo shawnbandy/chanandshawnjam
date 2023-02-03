aaf6b7728c23f17fad25b693da7390f899c2a263
Shawn Canavan <105885313+shawnbandy@users.noreply.github.com>
2023-02-03 18:33:12 -0500

    got the map to load

--- src/components/canvas.js
+++ src/components/canvas.js
@@ -1,9 +1,21 @@
 import React, { useRef, useEffect } from 'react';
+import PropTypes from 'prop-types';
 
 const Canvas = ({ draw, height, width }) => {
   const canvas = React.useRef();
 
+  React.useEffect(() => {
+    const ctx = canvas.current.getContext('2d');
+    draw(ctx);
+  });
+
   return <canvas ref={canvas} width={width} height={height}></canvas>;
+};
+
+Canvas.propTypes = {
+  draw: PropTypes.func.isRequired,
+  height: PropTypes.number.isRequired, // ADDED
+  width: PropTypes.number.isRequired, // ADDED
 };
 
 export default Canvas;
--- src/components/project.js
+++ src/components/project.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import Canvas from './canvas';
+import recovery from '../assets/images/recovery.png';
+
+const draw = async (c) => {
+  c.fillStyle = 'white';
+  c.fillRect(0, 0, 1024, 576);
+  const img = new Image();
+  img.src = recovery;
+  img.onload = () => {
+    c.drawImage(img, 0, 0);
+  };
+};
 
 function Project() {
   return (
     <section>
-      <Canvas width={1024} height={576} />
+      <Canvas draw={draw} width={1024} height={576} />
     </section>
   );
 }
