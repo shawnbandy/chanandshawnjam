93d419d06f03aef306ccbe3f919e5ed0670e4f33
Shawn Canavan <105885313+shawnbandy@users.noreply.github.com>
2023-02-04 13:27:01 -0500

    added character and changed map to be tiled for time being"

--- src/components/project.js
+++ src/components/project.js
@@ -1,18 +1,69 @@
 import React from 'react';
+import { useEffect } from 'react';
 import Canvas from './canvas';
 import recovery from '../assets/images/recovery.png';
+import recoveryTile from '../assets/images/recoveryTiles.png';
+import pU from '../assets/images/character/main_run_up.png';
+import pD from '../assets/images/character/main_run_down.png';
+import pR from '../assets/images/character/main_run_right.png';
+import pL from '../assets/images/character/main_run_left.png';
+import pI from '../assets/images/character/main_idle.png';
 
 const draw = async (c) => {
   c.fillStyle = 'white';
-  c.fillRect(0, 0, 1024, 576);
+  const height = 1024;
+  const width = 576;
+  c.fillRect(0, 0, height, width);
+
+  //*establishes the map and player
   const img = new Image();
-  img.src = recovery;
+  img.src = recoveryTile;
+  const playerImage = new Image();
+  playerImage.src = pI;
+  const playerWalk = new Image();
+  playerWalk.src = pD;
+
+  //*renders the map and player
   img.onload = () => {
     c.drawImage(img, 0, 0);
+    c.drawImage(
+      playerImage,
+      0,
+      0,
+      playerImage.width / 4.1,
+      playerImage.height,
+      height / 2 - playerImage.width / 3.3,
+      width / 2 - playerImage.height / 2,
+      playerImage.width / 4,
+      playerImage.height
+    );
   };
 };
 
+const renderMovement = () => {
+  window.requestAnimationFrame(renderMovement);
+};
+
+const keyDown = (e) => {
+  switch (e.key) {
+    case 'w':
+      break;
+    case 'a':
+      break;
+    case 's':
+      break;
+    case 'd':
+      break;
+    default:
+      break;
+  }
+};
+
 function Project() {
+  useEffect(() => {
+    document.addEventListener('keydown', keyDown);
+  });
+
   return (
     <section>
       <Canvas draw={draw} width={1024} height={576} />
